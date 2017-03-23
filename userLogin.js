844092c60ae02b37aff424ecb9c6be485e163344
Kevin <asdkevinasd@gmail.com>
2017-03-23 22:34:22 +0800

    password storing prototype changed into password hashing prototype

---
+++ userLogin.js
@@ -0,0 +1,24 @@
+/**
+ * Created by User on 22/03/2017.
+ */
+
+module.exports = {
+
+    init: function (res, password) {
+
+        //get saved hash and salt from the db with the input
+
+        //return error: username or password is wrong if username do not exist
+
+        //hash the input password with the saved salt and compare the two hash value
+
+        //return error: username or password is wrong if password do not exist
+
+        //check if two factor is enabled or not, if ys check for it
+
+        //create session if no error is thrown
+
+    }
+
+
+};
