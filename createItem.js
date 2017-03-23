7fd00c3b141b72cdfb12ac8e9c7899516defea4b
Kevin <asdkevinasd@gmail.com>
2017-03-23 23:26:05 +0800

    recommend prototype

---
+++ createItem.js
@@ -0,0 +1,12 @@
+/**
+ * Created by User on 23/03/2017.
+ */
+module.exports = {
+
+    init: function(res,itemName,catId,price,photoNum,itemDesc) {
+        //create record in itemdb with these info plus the userId
+
+        //return 1 for success or -1 for fail
+
+    }
+};
