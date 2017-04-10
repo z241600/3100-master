58d049cf58465438b3dca07cccda81ede0f136a1
Leo42111 <leo.ma4211@gmail.com>
2017-04-11 00:06:32 +0800

    Headache - dunno if code is done

--- search.js
+++ search.js
@@ -32,7 +32,7 @@ module.exports = {
         //lookup the target item in the db using priceRange and catalogId as parameter
         //return the itemId and other info if it exist
         //throw error:item not found and give Recommendation to user according to preference
-        //update the user's preference with the item's catalog index or the catalogId if item is not found
+        //update the user's preference with the item's catalog index or the catalogId if item is not foundzzz
     },
 
     //Search by price range
